15f973a2abfd3c1414dd867dfa390b5858d0e10f
shintan777 <tanvi.shinde46@gmail.com>
2020-05-15 00:41:03 +0530

    Adds random number generator

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // TODO: function getRandomNumber(min=0, max=1) --> shintan777
+function getRandomNumber(min = 0, max = 1) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
 // TODO: function getRandomBool() --> adi10hero
