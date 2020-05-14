96a730c93fde17019616afb99f3af7f06d6d8aea
Aditya Srivastava <adityasrivastava301199@gmail.com>
2020-05-15 01:40:10 +0530

    Minor Spacing changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function getRandomNumber(min = 0, max = 1, precision = 2) {
 function getRandomBool() {
     return getRandomNumber() >= 0.5;
 }
+
 function getRandomInt(min = 0, max = 1) {
-    return (Math.floor(getRandomNumber(min, max)));
+    return Math.floor(getRandomNumber(min, max));
 }
